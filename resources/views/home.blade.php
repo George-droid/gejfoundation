476a61b40eb9f5f6dddb9bb685d20640a10e525d
oti-favour <favourgraceoti@gmail.com>
2024-04-12 14:53:12 +0100

    page editing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -368,36 +368,33 @@
 
 
         <!-- Testimonial Start -->
-        <div class="container-xxl py-5">
+        <div class="container-xl py-5">
             <div class="container">
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                    <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Testimonial</div>
-                    <h1 class="display-6 mb-5">Trusted By Thousands Of People And Nonprofits</h1>
+                    <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Partners</div>
+                    <h1 class="display-7 mb-5">Trusted By Thousands Of People And Nonprofits</h1>
                 </div>
                 <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="img/testimonial-1.jpg" style="width: 100px; height: 100px;">
-                        <div class="testimonial-text rounded text-center p-4">
-                            <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-                            <h5 class="mb-1">Doner Name</h5>
-                            <span class="fst-italic">Profession</span>
-                        </div>
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" src="img/partner1.jpg">
                     </div>
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="img/testimonial-2.jpg" style="width: 100px; height: 100px;">
-                        <div class="testimonial-text rounded text-center p-4">
-                            <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-                            <h5 class="mb-1">Doner Name</h5>
-                            <span class="fst-italic">Profession</span>
-                        </div>
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" src="img/partner2.jpg">
                     </div>
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid bg-light rounded-circle p-2 mx-auto mb-4" src="img/testimonial-3.jpg" style="width: 100px; height: 100px;">
-                        <div class="testimonial-text rounded text-center p-4">
-                            <p>Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.</p>
-                            <h5 class="mb-1">Doner Name</h5>
-                            <span class="fst-italic">Profession</span>
-                        </div>
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" src="img/partner3.jpg">
+                    </div>
+                    <div class="testimonial-item text-center">
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" src="img/partner4.jpg">
+                    </div>
+                    <div class="testimonial-item text-center">
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" src="img/partner5.jpg">
+                    </div>
+                    <div class="testimonial-item text-center">
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" src="img/partner6.jpg">
+                    </div>
+                    <div class="testimonial-item text-center">
+                        <img class="img-fluid bg-light p-2 mx-auto mb-4" src="img/partner7.png">
                     </div>
                 </div>
             </div>
