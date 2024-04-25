ced03d33b5c9632c833b653741029994e46d03dd
oti-favour <favourgraceoti@gmail.com>
2024-04-25 11:28:55 +0100

    blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,130 +8,123 @@
             <div class="container mt-4">
                 <div class="row">
                   <!-- Main Content -->
-                  <div class="col-md-8">
-                        <!-- Blog Post -->
-                        <div class="card mb-4">
-                        <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
-                        <div class="card-body">
-                            <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                            <p class="card-text">August 6, 2023</p>
-                            <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
-                                Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
-                            <div class="card-footer">
-                            <a href="#" class="card-link">Likes:4</a>
-                            <a href="#" class="card-link">Comments: 3</a>
-                            </div>
-                        </div>
-                        </div>
+                    <div class="col-md-8">
                         <div class="card mb-4">
                             <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
                             <div class="card-body">
-                              <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                              <p class="card-text">August 6, 2023</p>
-                              <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
-                                  Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
-                              <div class="card-footer">
-                                <a href="#" class="card-link">Likes:4</a>
-                                <a href="#" class="card-link">Comments: 3</a>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card mb-4">
-                            <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
-                            <div class="card-body">
-                              <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                              <p class="card-text">August 6, 2023</p>
-                              <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
-                                  Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
-                              <div class="card-footer">
-                                <a href="#" class="card-link">Likes:4</a>
-                                <a href="#" class="card-link">Comments: 3</a>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card mb-4">
-                            <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
-                            <div class="card-body">
-                              <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                              <p class="card-text">August 6, 2023</p>
-                              <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
-                                  Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
-                              <div class="card-footer">
-                                <a href="#" class="card-link">Likes:4</a>
-                                <a href="#" class="card-link">Comments: 3</a>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card mb-4">
-                            <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
-                            <div class="card-body">
-                              <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                              <p class="card-text">August 6, 2023</p>
-                              <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
-                                  Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
-                              <div class="card-footer">
-                                <a href="#" class="card-link">Likes:4</a>
-                                <a href="#" class="card-link">Comments: 3</a>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card mb-4">
-                            <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
-                            <div class="card-body">
-                              <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                              <p class="card-text">August 6, 2023</p>
-                              <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
-                                  Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
-                              <div class="card-footer">
+                                <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
+                                <p class="card-text">August 6, 2023</p>
+                                <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
+                                    Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
+                                <div class="card-footer">
                                 <a href="#" class="card-link">Read More</a>
                                 <a href="#" class="card-link">Comments: 3</a>
-                              </div>
+                                </div>
                             </div>
-                          </div>
+                        </div>
+
+                        <div class="card mb-4">
+                            <img src="{{asset('img/courses-2.jpg')}}" class="card-img-top" alt="Post Image">
+                            <div class="card-body">
+                                <h5 class="card-title">Jonathan leads WAEF pre-election mission to Liberia</h5>
+                                <p class="card-text">July 11, 2023</p>
+                                <p class="card-text">Former President Dr. Goodluck Ebele Jonathan leads the West African Elders Forum (WAEF) pre-election election mission to Liberia ahead of the country’s general elections scheduled for October.
+
+                                Jonathan, who departed Abuja for Monrovia on Tuesday morning, is joined in the mission by former Burkinabe prime minister and one-time president of the Economic Community of West African States (ECOWAS) Kadre Ouedraogo and other leaders from the subregion. The mission, which also includes staff from the WAEF secretariat, will assess the preparedness of stakeholders ahead of the election.</p>
+                                <div class="card-footer">
+                                <a href="#" class="card-link">Read More</a>
+                                <a href="#" class="card-link">Comments: 3</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4">
+                            <img src="{{asset('img/courses-3.jpg')}}" class="card-img-top" alt="Post Image">
+                            <div class="card-body">
+                                <h5 class="card-title">WAEF commends Macky Sall for declining third term</h5>
+                                <p class="card-text">July 5, 2023</p>
+                                <p class="card-text">The West Africa Elders Forum (WAEF) has commended the president of Senegal Macky Sall for declining to run for a third in the 2024 presidential election. A decision the organization describes as sacrificial, one taken in the best interest of the nation.
+
+                                    Convener of the Forum and former President of Nigeria, Dr Goodluck Ebele Jonathan in a letter addressed to Macky Sall on behalf of the Forum referred to the President’s action as a display of exemplary leadership, a gesture worthy of emulation by other leaders.</p>
+                                <div class="card-footer">
+                                <a href="#" class="card-link">Read More</a>
+                                <a href="#" class="card-link">Comments: 3</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4">
+                            <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
+                            <div class="card-body">
+                                <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
+                                <p class="card-text">August 6, 2023</p>
+                                <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
+                                    Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
+                                <div class="card-footer">
+                                <a href="#" class="card-link">Likes:4</a>
+                                <a href="#" class="card-link">Comments: 3</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4">
+                            <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
+                            <div class="card-body">
+                                <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
+                                <p class="card-text">August 6, 2023</p>
+                                <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
+                                    Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
+                                <div class="card-footer">
+                                <a href="#" class="card-link">Read More</a>
+                                <a href="#" class="card-link">Comments: 3</a>
+                                </div>
+                            </div>
+                        </div>
                     
-                  </div>
-            
-                  <!-- Sidebar -->
-                  <div class="col-md-4">
-                    <!-- Categories Section -->
-                    <div class="card mb-4">
-                      <div class="card-header">Tags</div>
-                      <div class="card-body">
-                        <ul class="list-unstyled">
-                          <li><a href="#">Abuja</a></li>
-                          <li><a href="#">Africa</a></li>
-                          <li><a href="#">Burkina Faso</a></li>
-                          <li><a href="#">Civic Participation</a></li>
-                          <li><a href="#">Commitment</a></li>
-                          <li><a href="#">Consensus</a></li>
-                          <li><a href="#">Constitutional Review</a></li>
-                          <li><a href="#">Democracy</a></li>
-                          <li><a href="#">Democractic Principles</a></li>
-                          <li><a href="#">Dialogue</a></li>
-                          <li><a href="#">Dr. Goodluck Jonathan </a></li>
-                          <li><a href="#">ECOWAS</a></li>
-                          <li><a href="#"> Election</a></li>
-                          <li><a href="#">Ernest Bai Koroma</a></li>
-                        </ul>
-                      </div>
                     </div>
-            
-                    <!-- Authors Section -->
-                    <div class="card mb-4">
-                      <div class="card-header">Authors</div>
-                      <div class="card-body">
-                        <ul class="list-unstyled">
-                          <li><a href="#">Author 1</a></li>
-                          <li><a href="#">Author 2</a></li>
-                          <li><a href="#">Author 3</a></li>
-                          <li><a href="#">Author 4</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
+                    
+                    <!-- Sidebar -->
+                        <div class="col-md-4">
+                            <!-- Categories Section -->
+                            <div class="card mb-4">
+                            <div class="card-header">Tags</div>
+                            <div class="card-body">
+                                <ul class="list-unstyled">
+                                <li><a href="#">Abuja</a></li>
+                                <li><a href="#">Africa</a></li>
+                                <li><a href="#">Burkina Faso</a></li>
+                                <li><a href="#">Civic Participation</a></li>
+                                <li><a href="#">Commitment</a></li>
+                                <li><a href="#">Consensus</a></li>
+                                <li><a href="#">Constitutional Review</a></li>
+                                <li><a href="#">Democracy</a></li>
+                                <li><a href="#">Democractic Principles</a></li>
+                                <li><a href="#">Dialogue</a></li>
+                                <li><a href="#">Dr. Goodluck Jonathan </a></li>
+                                <li><a href="#">ECOWAS</a></li>
+                                <li><a href="#"> Election</a></li>
+                                <li><a href="#">Ernest Bai Koroma</a></li>
+                                </ul>
+                            </div>
+                            </div>
+                    
+                            <!-- Authors Section -->
+                            <div class="card mb-4">
+                            <div class="card-header">Authors</div>
+                            <div class="card-body">
+                                <ul class="list-unstyled">
+                                <li><a href="#">Author 1</a></li>
+                                <li><a href="#">Author 2</a></li>
+                                <li><a href="#">Author 3</a></li>
+                                <li><a href="#">Author 4</a></li>
+                                </ul>
+                            </div>
+                            </div>
+                        </div>
+                    
                 </div>
-              </div>
-              <!-- End Main Content and Sidebar -->
+            </div>
+            
         </div>
     </div>
 </body>
