0f2dac097e66313f90ed2d1ccc1ab425e9e8dcc0
oti-favour <favourgraceoti@gmail.com>
2024-04-25 09:41:27 +0100

    blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -29,13 +29,23 @@
                   <div class="col-md-4">
                     <!-- Categories Section -->
                     <div class="card mb-4">
-                      <div class="card-header">Categories</div>
+                      <div class="card-header">Tags</div>
                       <div class="card-body">
                         <ul class="list-unstyled">
-                          <li><a href="#">Category 1</a></li>
-                          <li><a href="#">Category 2</a></li>
-                          <li><a href="#">Category 3</a></li>
-                          <li><a href="#">Category 4</a></li>
+                          <li><a href="#">Abuja</a></li>
+                          <li><a href="#">Africa</a></li>
+                          <li><a href="#">Burkina Faso</a></li>
+                          <li><a href="#">Civic Participation</a></li>
+                          <li><a href="#">Commitment</a></li>
+                          <li><a href="#">Consensus</a></li>
+                          <li><a href="#">Constitutional Review</a></li>
+                          <li><a href="#">Democracy</a></li>
+                          <li><a href="#">Democractic Principles</a></li>
+                          <li><a href="#">Dialogue</a></li>
+                          <li><a href="#">Dr. Goodluck Jonathan </a></li>
+                          <li><a href="#">ECOWAS</a></li>
+                          <li><a href="#"> Election</a></li>
+                          <li><a href="#">Ernest Bai Koroma</a></li>
                         </ul>
                       </div>
                     </div>
