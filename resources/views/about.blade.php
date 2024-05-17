ca440ca1aa8a9ea1963c8aa8e0df46b9b54eff6d
oti-favour <favourgraceoti@gmail.com>
2024-05-17 12:25:09 +0100

    corrections

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,48 @@
 
 @section('content')
     <body>
+               <!-- About Start -->
+               <div class="container-xxl py-5">
+                <div class="container">
+                    <div class="row g-5">
+                        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                            <div class="position-relative overflow-hidden h-100" style="min-height: 400px;">
+                                <img class="position-absolute w-100 h-100 pt-5 pe-5" src="{{asset('img/2.jpeg')}}" alt="" style="object-fit: cover;">
+                                {{-- <img class="position-absolute top-0 end-0 bg-white ps-2 pb-2" src="{{asset('img/mission.png')}}" alt="" style="width: 200px; height: 200px;"> --}}
+                            </div>
+                        </div>
+                        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                            <div class="h-100">
+                                <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Who We Are</div>
+                                <h1 class="display-6 mb-2">We Promote Peace and Prosperity Around the World</h1>
+                                <div class="bg-light border-bottom border-5 border-primary rounded p-4 mb-4">
+                                    <p class="text-dark mb-2">
+                                        We understand that preserving and upholding democratic principles are prerequisites for any peaceful and prosperous nation.</p>
+                                    <span class="text-primary">Goodluck Ebele Jonathan, Founder</span>
+                                </div>
+                                <p class="mb-5">Through initiatives such as electoral reform, enhancing women and youth participation in elections, and mobilizing for violence-free elections in Nigeria, we aim to create positive change. Additionally, we are committed to nurturing 10,000 detribalized and emotionally resilient Nigerian children. Together, we're building a brighter future for Africa.</p>
+                                <div class="d-flex">
+                                    <a class="btn btn-primary py-2 px-3 me-3" href="">
+                                        Learn More
+                                        <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                                            <i class="fa fa-arrow-right"></i>
+                                        </div>
+                                    </a>
+                                    <a class="btn btn-outline-primary px-3" href="">
+                                        Contact Us
+                                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                                            <i class="fa fa-arrow-right"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- About End -->
+
         <div class="container-fluid donate my-2 py-5" data-parallax="scroll" data-image-src="img/carousel-2.jpg">
             <div class="container py-5">
                 <div class="row g-5 align-items-center">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,45 +60,77 @@
         </div>
         <!-- Carousel End -->
 
-
-        <!-- About Start -->
         <div class="container-xxl py-5">
             <div class="container">
-                <div class="row g-5">
-                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="position-relative overflow-hidden h-100" style="min-height: 400px;">
-                            <img class="position-absolute w-100 h-100 pt-5 pe-5" src="{{asset('img/2.jpeg')}}" alt="" style="object-fit: cover;">
-                            {{-- <img class="position-absolute top-0 end-0 bg-white ps-2 pb-2" src="{{asset('img/mission.png')}}" alt="" style="width: 200px; height: 200px;"> --}}
-                        </div>
-                    </div>
-                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-                        <div class="h-100">
-                            <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Who We Are</div>
-                            <h1 class="display-6 mb-2">We Promote Peace and Prosperity Around the World</h1>
-                            <div class="bg-light border-bottom border-5 border-primary rounded p-4 mb-4">
-                                <p class="text-dark mb-2">
-                                    We understand that preserving and upholding democratic principles are prerequisites for any peaceful and prosperous nation.</p>
-                                <span class="text-primary">Goodluck Ebele Jonathan, Founder</span>
-                            </div>
-                            <p class="mb-5">Through initiatives such as electoral reform, enhancing women and youth participation in elections, and mobilizing for violence-free elections in Nigeria, we aim to create positive change. Additionally, we are committed to nurturing 10,000 detribalized and emotionally resilient Nigerian children. Together, we're building a brighter future for Africa.</p>
-                            <a class="btn btn-primary py-2 px-3 me-3" href="">
+                <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                    <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">About Us</div>
+                    <h1 class="display-6 mb-5"></h1>
+                </div>
+                <div class="row g-4 justify-content-center">
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                            <img class="img-fluid mb-4" src="{{asset('img/corevalue.png')}}" alt="">
+                            <h4 class="mb-3">Our Core Values</h4>
+                            <p class="mb-4">Transparency and accountability, Equity & Fairness, Respect, Integrity, Inclusiveness, Independence, Professionalism.</p>
+                            {{-- <a class="btn btn-outline-primary px-3" href="">
                                 Learn More
-                                <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                            <a class="btn btn-outline-primary py-2 px-3" href="">
-                                Contact Us
                                 <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
                                     <i class="fa fa-arrow-right"></i>
                                 </div>
-                            </a>
+                            </a> --}}
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                        <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                            <img class="img-fluid mb-4" src="img/vision.png" alt="">
+                            <h4 class="mb-3">Our Vision</h4>
+                            <p class="mb-4">Peaceful, Just and Progressive Nations of Africa</p>
+                            {{-- <a class="btn btn-outline-primary px-3" href="">
+                                Learn More
+                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                                    <i class="fa fa-arrow-right"></i>
+                                </div>
+                            </a> --}}
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                            <img class="img-fluid mb-4" src="img/mission.png" alt="">
+                            <h4 class="mb-3">Our Mission</h4>
+                            <p class="mb-4">To forge a continent where we understand our differences and work towards a perfect union founded on transparency, equity and justice</p>
+                            {{-- <a class="btn btn-outline-primary px-3" href="">
+                                Learn More
+                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                                    <i class="fa fa-arrow-right"></i>
+                                </div>
+                            </a> --}}
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid donate my-5 py-5" data-parallax="scroll" data-image-src="img/carousel-2.jpg">
+            <div class="container py-5">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                        <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Watch Now</div>
+                        <h1 class="display-6 text-white mb-5">DOCUMENTARY ON RESPECT FOR CONSTITUTIONAL TERM LIMITS IN WEST AFRICA</h1>
+                        <p class="text-white mb-0">The question of tenure elongation has moved from the sphere of normal devolution of power to the transitional context. It has become the main stake of the recent military-political transitions in West Africa.
+
+                            The different coups d’état perpetrated by military elites in Mali (2020, 2021), Guinea (2021) and Burkina-Faso (2022) point out how complicated the allocation of a relevant time for a successful political transition is. It appears that the sanctions are no longer effective in deterring military coups or prolonging the stay of transition governments …</p>
+                    </div>
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                        <div class="h-100 bg-white p-5">
+                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/CmORN8A1MnA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- About End -->
+
+ 
 
 
         <!-- News and events Start -->
@@ -213,84 +245,6 @@
         </div>
         <!-- News and events End -->
 
-
-        <!-- Service Start -->
-        <div class="container-xxl py-5">
-            <div class="container">
-                <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                    <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">What We Do</div>
-                    <h1 class="display-6 mb-5">Learn More What We Do And Get Involved</h1>
-                </div>
-                <div class="row g-4 justify-content-center">
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                            <img class="img-fluid mb-4" src="{{asset('img/corevalue.png')}}" alt="">
-                            <h4 class="mb-3">Our Core Values</h4>
-                            <p class="mb-4">Transparency and accountability, Equity & Fairness, Respect, Integrity, Inclusiveness, Independence, Professionalism.</p>
-                            {{-- <a class="btn btn-outline-primary px-3" href="">
-                                Learn More
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a> --}}
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                        <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                            <img class="img-fluid mb-4" src="img/vision.png" alt="">
-                            <h4 class="mb-3">Our Vision</h4>
-                            <p class="mb-4">Peaceful, Just and Progressive Nations of Africa</p>
-                            {{-- <a class="btn btn-outline-primary px-3" href="">
-                                Learn More
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a> --}}
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                            <img class="img-fluid mb-4" src="img/mission.png" alt="">
-                            <h4 class="mb-3">Our Mission</h4>
-                            <p class="mb-4">To forge a continent where we understand our differences and work towards a perfect union founded on transparency, equity and justice</p>
-                            {{-- <a class="btn btn-outline-primary px-3" href="">
-                                Learn More
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a> --}}
-                        </div>
-                    </div>
-                  
-                </div>
-            </div>
-        </div>
-        <!-- Service End -->
-
-
-     
-        <div class="container-fluid donate my-5 py-5" data-parallax="scroll" data-image-src="img/carousel-2.jpg">
-            <div class="container py-5">
-                <div class="row g-5 align-items-center">
-                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                        <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Watch Now</div>
-                        <h1 class="display-6 text-white mb-5">DOCUMENTARY ON RESPECT FOR CONSTITUTIONAL TERM LIMITS IN WEST AFRICA</h1>
-                        <p class="text-white mb-0">The question of tenure elongation has moved from the sphere of normal devolution of power to the transitional context. It has become the main stake of the recent military-political transitions in West Africa.
-
-                            The different coups d’état perpetrated by military elites in Mali (2020, 2021), Guinea (2021) and Burkina-Faso (2022) point out how complicated the allocation of a relevant time for a successful political transition is. It appears that the sanctions are no longer effective in deterring military coups or prolonging the stay of transition governments …</p>
-                    </div>
-                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                        <div class="h-100 bg-white p-5">
-                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/CmORN8A1MnA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Donate End -->
-
-
-     
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
