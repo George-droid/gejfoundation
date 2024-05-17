0b5c3d596388f5b5e25c9371909be9e55c4c20cc
oti-favour <favourgraceoti@gmail.com>
2024-05-17 14:44:45 +0100

    corrections

--- resources/views/waefworks.blade.php
+++ resources/views/waefworks.blade.php
@@ -6,29 +6,16 @@
         
         <div class="container py-5">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Latest News</div>
-                <h1 class="display-6 mb-5">All News And Media</h1>
+                <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Our Works</div>
+                <h1 class="display-6 mb-5">WAEF News And Media</h1>
             </div>
             <div class="container mt-4">
                 <div class="row">
                   <!-- Main Content -->
                     <div class="col-md-8">
+                       
                         <div class="card mb-4">
-                            <img src="{{asset('img/courses-1.jpg')}}" class="card-img-top" alt="Post Image">
-                            <div class="card-body">
-                                <h5 class="card-title">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                                <p class="card-text">August 6, 2023</p>
-                                <p class="card-text">A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)
-                                    Recent happenings in West Africa points the fact of the danger democracy and good governance are facing in the region. The military takeover in Mali, Chad, Burkina Faso, Sudan and Niger Republic showed the fragility of moving West Africa out of its challenge of poverty, low infrastructure and globalisation.</p>
-                                <div class="card-footer">
-                                <a href="#" class="card-link">Read More</a>
-                                <a href="#" class="card-link">Comments: 3</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card mb-4">
-                            <img src="{{asset('img/courses-2.jpg')}}" class="card-img-top" alt="Post Image">
+                            <img src="{{asset('img/courses-2.jpg')}}" class="card-img-top " alt="Post Image">
                             <div class="card-body">
                                 <h5 class="card-title">Jonathan leads WAEF pre-election mission to Liberia</h5>
                                 <p class="card-text">July 11, 2023</p>
