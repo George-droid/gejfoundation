766f1f3a6f3783225ae4373928f03a85d1a369d7
oti-favour <favourgraceoti@gmail.com>
2024-05-20 15:52:10 +0100

    corrections

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -23,7 +23,37 @@
                 <p>Brief description of the resource content. This can include what the document or link is about and why it might be useful to the visitor.</p>
                 <a href="resource-link3.pdf" class="btn btn-primary">Download</a>
             </li>
-            <!-- Add more resources as needed -->
+            <li>
+                <h5>Resource Title 1</h5>
+                <p>Brief description of the resource content. This can include what the document or link is about and why it might be useful to the visitor.</p>
+                <a href="resource-link1.pdf" class="btn btn-primary">Download</a>
+            </li>
+            <li>
+                <h5>Resource Title 2</h5>
+                <p>Brief description of the resource content. This can include what the document or link is about and why it might be useful to the visitor.</p>
+                <a href="resource-link2.pdf" class="btn btn-primary">Download</a>
+            </li>
+            <li>
+                <h5>Resource Title 3</h5>
+                <p>Brief description of the resource content. This can include what the document or link is about and why it might be useful to the visitor.</p>
+                <a href="resource-link3.pdf" class="btn btn-primary">Download</a>
+            </li>
+            <li>
+                <h5>Resource Title 1</h5>
+                <p>Brief description of the resource content. This can include what the document or link is about and why it might be useful to the visitor.</p>
+                <a href="resource-link1.pdf" class="btn btn-primary">Download</a>
+            </li>
+            <li>
+                <h5>Resource Title 2</h5>
+                <p>Brief description of the resource content. This can include what the document or link is about and why it might be useful to the visitor.</p>
+                <a href="resource-link2.pdf" class="btn btn-primary">Download</a>
+            </li>
+            <li>
+                <h5>Resource Title 3</h5>
+                <p>Brief description of the resource content. This can include what the document or link is about and why it might be useful to the visitor.</p>
+                <a href="resource-link3.pdf" class="btn btn-primary">Download</a>
+            </li>
+           
         </ul>
     </div>
 </body>
