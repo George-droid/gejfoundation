f334c7815abb8589e8ed0db3ebe2459de38696c3
Wisdom George <wisgeorge.wg@gmail.com>
2024-05-27 09:58:39 +0100

    Added socials to about security page

--- resources/views/security.blade.php
+++ resources/views/security.blade.php
@@ -103,6 +103,25 @@
                 </div>
             </div>
         </div>
+
+        <!-- Team Socuals -->
+        <div>
+            <div class="container-xxl py-5">
+                <div class="text-center mx-auto mb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                    <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Our Socials</div>
+                </div>
+                <div class="row g-4 justify-content-center"> 
+                    <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgejfoundation&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
+                            width="340" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    </div>
+                    <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.15s">
+                        <a class="twitter-timeline"  href="https://twitter.com/Wisdomgeo_rge?ref_src=twsrc%5Etfw">Tweets by Wisdomgeo_rge</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                    </div>
+                </div>
+            </div>                 
+        </div>
+        <!-- Team Socials -->
     </body>
 @endsection
 </html>
