cdc2e7963f20f99406069996c3c286ec3927f237
oti-favour <favourgraceoti@gmail.com>
2024-05-30 10:12:41 +0100

    hero section

--- resources/views/gejgallery.blade.php
+++ resources/views/gejgallery.blade.php
@@ -29,45 +29,9 @@
                           <li class="page-item"><a class="page-link" href="#">Work</a></li>
                         </ul>
                     </nav>
-            <!-- Carousel for Image Groups -->
-                    {{-- <div id="imageGroupsCarousel" class="carousel slide" data-bs-ride="carousel"> --}}
-                        {{-- <div class="carousel-inner">
-                            <div class="row my-4">
-                                <!-- Column for gallery images -->
-                                <div class="col-md-2 mb-4">
-                                    <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 1">
-                                </div>
-                                <div class="col-md-2 mb-4">
-                                    <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 2">
-                                </div>
-                                <div class="col-md-2 mb-4">
-                                    <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 3">
-                                </div>
-                                <div class="col-md-2 mb-4">
-                                    <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 3">
-                                </div>
-                                <div class="col-md-2 mb-4">
-                                    <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 3">
-                                </div>
-                                <div class="col-md-2 mb-4">
-                                    <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 3">
-                                </div>
-                                <!-- Add more images as needed -->
-                            </div>
-                        </div> --}}
-                        
-                        <!-- Carousel controls -->
-                        {{-- <button class="carousel-control-prev" type="button" data-bs-target="#imageGroupsCarousel" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#imageGroupsCarousel" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div> --}}
+          
 
-                    <div class="container py-5">
+                    <div class="container py-3">
                         <div class="text-center mb-5">
                             <h1 class="display-6">Foundation Gallery</h1>
                         </div>
@@ -123,31 +87,31 @@
                     <!-- Gallery Section -->
                     <div class="row">
                         <!-- Column for gallery images -->
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 1">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 2">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 3">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 1">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 2">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 3">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 1">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 2">
                         </div>
-                        <div class="col-md-4 mb-4">
+                        <div class="col-md-3 mb-4">
                             <img src="https://via.placeholder.com/300x200" class="img-fluid" alt="Image 3">
                         </div>
                         
