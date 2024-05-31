84e9daee9c54a84a0e838a727c375adf6d798f82
oti-favour <favourgraceoti@gmail.com>
2024-05-31 12:53:39 +0100

    corrections

--- resources/views/gejgallery.blade.php
+++ resources/views/gejgallery.blade.php
@@ -4,15 +4,14 @@
 
 
 <body>
-    <div class="container mx-5">
-       
-            <div class="container py-3">
+    <div class="container">
+            <div class="container bg-light my-2 py-2">
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
                     <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Gallery</div>
                     <h1 class="display-6 mb-5">Look Through our Events and Activities</h1>
                 </div>
                 
-                <nav aria-label="Page navigation example">
+                <nav aria-label="Page navigation example" class="d-none d-lg-block">
                     <ul class="pagination justify-content-center mb-5">
                         <li class="page-item"><a class="page-link filter-link" data-category="All" href="#">All</a></li>
                         <li class="page-item"><a class="page-link filter-link" data-category="Events" href="#">Events</a></li>
@@ -30,9 +29,9 @@
                     <div class="text-center mb-5">
                         <h1 class="display-6">Foundation Gallery</h1>
                     </div>
-                    <div class="gallery-container row">
+                    <div class="gallery-container row gx-4">
                         <!-- Gallery Item 1 -->
-                        <div class="gallery-item col-lg-4 col-md-6 mb-4" data-images="https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF">
+                        <div class="gallery-item col-lg-4 col-md-6 mb-4">
                             <div class="position-relative mt-auto">
                                 <img src="https://via.placeholder.com/400x300/FFFFFF" class="img-fluid border border-success" alt="Gallery Image 1">
                                 <div class="overlay">
@@ -47,7 +46,7 @@
                             </div>
                         </div>
                         <!-- Gallery Item 2 -->
-                        <div class="gallery-item col-lg-4 col-md-6 mb-4" data-images="https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF">
+                        <div class="gallery-item col-lg-4 col-md-6 mb-4">
                             <div class="position-relative mt-auto">
                                 <img src="https://via.placeholder.com/400x300/FFFFFF" class="img-fluid border border-success" alt="Gallery Image 2">
                                 <div class="overlay">
@@ -62,7 +61,7 @@
                             </div>
                         </div>
                         <!-- Gallery Item 3 -->
-                        <div class="gallery-item col-lg-4 col-md-6 mb-4" data-images="https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF, https://via.placeholder.com/300x200/FFFFFF">
+                        <div class="gallery-item col-lg-4 col-md-6 mb-4">
                             <div class="position-relative mt-auto">
                                 <img src="https://via.placeholder.com/400x300/FFFFFF" class="img-fluid border border-success" alt="Gallery Image 3">
                                 <div class="overlay">
@@ -78,6 +77,7 @@
                         </div>
                     </div>
                 </div>
+                
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
                     <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Recents</div>
                 </div>
